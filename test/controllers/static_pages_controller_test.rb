ebee3d39008a5405c5507e58bf6509b6e7c027e5
Ganglians <jchav027@ucr.edu>
2020-06-22 10:15:16 -0700

    [1] Customize the HTML in blog and home views. [2] Add line to home and blog views to verify that they display the correct title (red). [3] Implement full HTML structure in blog and home views. [4] Add titles to home and blog views (tests from -2- are green now).

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    # To avoid repetition, define part of the title referenced more than once
+    @base_title = "| Syte"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home #{@base_title}"
   end
 
   test "should get blog" do
     get static_pages_blog_url
     assert_response :success
+    assert_select "title", "Blog #{@base_title}"
   end
-
 end
