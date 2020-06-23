d8a5b57513aac79f5e2d5e24e3f074bcc0b53a17
Ganglians <jchav027@ucr.edu>
2020-06-23 10:51:35 -0700

    Default title: [1] Define a custom helper method in the ‘application_helper.rb’ file that provides a default page title in the absence of a page-specific title. [2] Remove the page-specific title ‘Home’ from ‘home.html.erb’. There’s no need for it because it’s already set as the root route; it’s widely known that the home page is what one sees when visiting a website’s default URL. [3] Replace <title> tag’s contents in the ‘application.html.erb’ layout file with a call to the title helper method. [4] Update static pages controller tests to reflect these changes.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     # DRY what the titles share in common
-    @base_title = "| Syte"
+    @base_title = "Syte"
   end
 
   test "should get root" do
     get root_url # root is static pages > home
     assert_response :success
-    assert_select "title", "Home #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get blog" do
     get static_pages_blog_url
     assert_response :success
-    assert_select "title", "Blog #{@base_title}"
+    assert_select "title", "Blog | #{@base_title}"
   end
 end
